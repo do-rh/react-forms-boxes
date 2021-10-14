de8f40161135133bd750aea3c32e5278511b7a5a
do-rh <87504110+do-rh@users.noreply.github.com>
2021-10-14 12:56:21 -0700

    added Box component and completed component chains

--- src/Box.js
+++ src/Box.js
@@ -1,9 +1,22 @@
-function Box({ deleteBox }) { }
+/** Renders a box with specified dimensions and color
+ *  Props: w, h, color, id, deleteBox function
+ *  State: none
+ */
+function Box({ w, h, color, id, deleteBox }) {
+  const boxDims = {
+    "width": `${w}px`,
+    "height": `${h}px`,
+    "backgroundColor": color,
+  };
 
+  return (
+    <div>
+      <div className="box" style={boxDims} name={id}>
+      </div>
+      <button onClick={(evt) => deleteBox(id)}> Delete me!</button>
+    </div>
+  );
 
-onclick(
-  const id = evt.target.id;
-deleteBox(id)
-)
+}
 
 export default Box;
